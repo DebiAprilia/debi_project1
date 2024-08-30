858b4e72481f2ea5cd61feb375a056bcc159fbda
Debi Aprilia <biiixprl1304@gmail.com>
2024-08-30 23:17:32 +0700

    Tugas PBO Minggu 2

---
+++ proyek1/Debi_TugasPBO2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.debi_project1;
+
+/**
+ *
+ * @author user
+ */
+public class Debi_TugasPBO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama   : Debi Aprilia");
+        System.out.println("NPM    : 2320506038");
+        System.out.println("Alamat : Kranggan, Banyurojo, Mertoyudan, Magelang");
+        System.out.println("No_Hp  : 08195107123");
+    }
+}
